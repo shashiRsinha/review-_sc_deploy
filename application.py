bb6eeb335fcd08a987b8f9f69a4a4aafcdcd9e0e
shashiRsinha <81066918+shashiRsinha@users.noreply.github.com>
2023-03-02 19:03:25 +0530

    Update application.py

--- application.py
+++ application.py
@@ -71,10 +71,7 @@ def index():
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://shashiranjan:dhanbad123@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
-            db = client['review_scrap']
-            review_col = db['review_scrap_data']
-            review_col.insert_many(reviews)
+
             return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             print('The Exception message is: ',e)
